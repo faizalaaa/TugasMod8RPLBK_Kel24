3559d5aacfac8a274b4a45a2d3678e3693e97b03
Ijal <faizal.ardhani12@gmail.com>
2023-11-16 22:30:33 +0700

    First commit

---
+++ perc2mod8/Form1.Designer.vb
@@ -0,0 +1,301 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        TextBox1 = New TextBox()
+        Button1 = New Button()
+        Button2 = New Button()
+        Button3 = New Button()
+        Button4 = New Button()
+        Button5 = New Button()
+        Button6 = New Button()
+        Button7 = New Button()
+        Button8 = New Button()
+        Button9 = New Button()
+        Button10 = New Button()
+        Button11 = New Button()
+        Button12 = New Button()
+        TextBox2 = New TextBox()
+        Label3 = New Label()
+        Label4 = New Label()
+        TableLayoutPanel1 = New TableLayoutPanel()
+        TableLayoutPanel1.SuspendLayout()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Montserrat", 12.0F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.ForeColor = SystemColors.ControlText
+        Label1.Location = New Point(255, 20)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(285, 24)
+        Label1.TabIndex = 0
+        Label1.Text = "Tugas Modul 8 Kelompok 24"
+        ' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Montserrat", 7.79999971F, FontStyle.Regular, GraphicsUnit.Point)
+        Label2.Location = New Point(184, 81)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(149, 16)
+        Label2.TabIndex = 1
+        Label2.Text = "Masukkan Suhu Awal"
+        ' 
+        ' TextBox1
+        ' 
+        TextBox1.Location = New Point(169, 104)
+        TextBox1.Margin = New Padding(3, 4, 3, 4)
+        TextBox1.Name = "TextBox1"
+        TextBox1.Size = New Size(164, 27)
+        TextBox1.TabIndex = 2
+        ' 
+        ' Button1
+        ' 
+        Button1.ForeColor = Color.Red
+        Button1.Location = New Point(384, 4)
+        Button1.Margin = New Padding(3, 4, 3, 4)
+        Button1.Name = "Button1"
+        Button1.Size = New Size(86, 31)
+        Button1.TabIndex = 5
+        Button1.Text = "C To R"
+        Button1.UseVisualStyleBackColor = True
+        ' 
+        ' Button2
+        ' 
+        Button2.ForeColor = Color.Red
+        Button2.Location = New Point(384, 81)
+        Button2.Margin = New Padding(3, 4, 3, 4)
+        Button2.Name = "Button2"
+        Button2.Size = New Size(86, 31)
+        Button2.TabIndex = 6
+        Button2.Text = "C To F"
+        Button2.UseVisualStyleBackColor = True
+        ' 
+        ' Button3
+        ' 
+        Button3.ForeColor = Color.Red
+        Button3.Location = New Point(384, 158)
+        Button3.Margin = New Padding(3, 4, 3, 4)
+        Button3.Name = "Button3"
+        Button3.Size = New Size(86, 31)
+        Button3.TabIndex = 7
+        Button3.Text = "C To K"
+        Button3.UseVisualStyleBackColor = True
+        ' 
+        ' Button4
+        ' 
+        Button4.ForeColor = Color.ForestGreen
+        Button4.Location = New Point(267, 4)
+        Button4.Margin = New Padding(3, 4, 3, 4)
+        Button4.Name = "Button4"
+        Button4.Size = New Size(86, 31)
+        Button4.TabIndex = 8
+        Button4.Text = "R To C"
+        Button4.UseVisualStyleBackColor = True
+        ' 
+        ' Button5
+        ' 
+        Button5.ForeColor = Color.ForestGreen
+        Button5.Location = New Point(267, 81)
+        Button5.Margin = New Padding(3, 4, 3, 4)
+        Button5.Name = "Button5"
+        Button5.Size = New Size(86, 31)
+        Button5.TabIndex = 9
+        Button5.Text = "R To F"
+        Button5.UseVisualStyleBackColor = True
+        ' 
+        ' Button6
+        ' 
+        Button6.ForeColor = Color.ForestGreen
+        Button6.Location = New Point(267, 158)
+        Button6.Margin = New Padding(3, 4, 3, 4)
+        Button6.Name = "Button6"
+        Button6.Size = New Size(86, 31)
+        Button6.TabIndex = 10
+        Button6.Text = "R To K"
+        Button6.UseVisualStyleBackColor = True
+        ' 
+        ' Button7
+        ' 
+        Button7.Location = New Point(152, 4)
+        Button7.Margin = New Padding(3, 4, 3, 4)
+        Button7.Name = "Button7"
+        Button7.Size = New Size(86, 31)
+        Button7.TabIndex = 11
+        Button7.Text = "F To C"
+        Button7.UseVisualStyleBackColor = True
+        ' 
+        ' Button8
+        ' 
+        Button8.Location = New Point(152, 81)
+        Button8.Margin = New Padding(3, 4, 3, 4)
+        Button8.Name = "Button8"
+        Button8.Size = New Size(86, 31)
+        Button8.TabIndex = 12
+        Button8.Text = "F To R"
+        Button8.UseVisualStyleBackColor = True
+        ' 
+        ' Button9
+        ' 
+        Button9.Location = New Point(152, 158)
+        Button9.Margin = New Padding(3, 4, 3, 4)
+        Button9.Name = "Button9"
+        Button9.Size = New Size(86, 31)
+        Button9.TabIndex = 13
+        Button9.Text = "F To K"
+        Button9.UseVisualStyleBackColor = True
+        ' 
+        ' Button10
+        ' 
+        Button10.ForeColor = Color.RoyalBlue
+        Button10.Location = New Point(32, 4)
+        Button10.Margin = New Padding(3, 4, 3, 4)
+        Button10.Name = "Button10"
+        Button10.Size = New Size(86, 31)
+        Button10.TabIndex = 14
+        Button10.Text = "K To C"
+        Button10.UseVisualStyleBackColor = True
+        ' 
+        ' Button11
+        ' 
+        Button11.ForeColor = Color.RoyalBlue
+        Button11.Location = New Point(32, 81)
+        Button11.Margin = New Padding(3, 4, 3, 4)
+        Button11.Name = "Button11"
+        Button11.Size = New Size(86, 31)
+        Button11.TabIndex = 15
+        Button11.Text = "K To R"
+        Button11.UseVisualStyleBackColor = True
+        ' 
+        ' Button12
+        ' 
+        Button12.ForeColor = Color.RoyalBlue
+        Button12.Location = New Point(32, 158)
+        Button12.Margin = New Padding(3, 4, 3, 4)
+        Button12.Name = "Button12"
+        Button12.Size = New Size(86, 31)
+        Button12.TabIndex = 16
+        Button12.Text = "K To F"
+        Button12.UseVisualStyleBackColor = True
+        ' 
+        ' TextBox2
+        ' 
+        TextBox2.Location = New Point(440, 101)
+        TextBox2.Margin = New Padding(3, 4, 3, 4)
+        TextBox2.Name = "TextBox2"
+        TextBox2.Size = New Size(164, 27)
+        TextBox2.TabIndex = 17
+        ' 
+        ' Label3
+        ' 
+        Label3.AutoSize = True
+        Label3.Font = New Font("Montserrat", 9.0F, FontStyle.Regular, GraphicsUnit.Point)
+        Label3.Location = New Point(440, 81)
+        Label3.Name = "Label3"
+        Label3.Size = New Size(116, 19)
+        Label3.TabIndex = 18
+        Label3.Text = "Hasil Konversi"
+        ' 
+        ' Label4
+        ' 
+        Label4.AutoSize = True
+        Label4.Location = New Point(348, 108)
+        Label4.Name = "Label4"
+        Label4.Size = New Size(0, 20)
+        Label4.TabIndex = 19
+        ' 
+        ' TableLayoutPanel1
+        ' 
+        TableLayoutPanel1.ColumnCount = 4
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 50.622406F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Percent, 49.377594F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 120.0F))
+        TableLayoutPanel1.ColumnStyles.Add(New ColumnStyle(SizeType.Absolute, 120.0F))
+        TableLayoutPanel1.Controls.Add(Button1, 0, 0)
+        TableLayoutPanel1.Controls.Add(Button2, 0, 1)
+        TableLayoutPanel1.Controls.Add(Button3, 0, 2)
+        TableLayoutPanel1.Controls.Add(Button4, 1, 0)
+        TableLayoutPanel1.Controls.Add(Button12, 3, 2)
+        TableLayoutPanel1.Controls.Add(Button5, 1, 1)
+        TableLayoutPanel1.Controls.Add(Button11, 3, 1)
+        TableLayoutPanel1.Controls.Add(Button6, 1, 2)
+        TableLayoutPanel1.Controls.Add(Button10, 3, 0)
+        TableLayoutPanel1.Controls.Add(Button7, 2, 0)
+        TableLayoutPanel1.Controls.Add(Button9, 2, 2)
+        TableLayoutPanel1.Controls.Add(Button8, 2, 1)
+        TableLayoutPanel1.ForeColor = Color.Coral
+        TableLayoutPanel1.Location = New Point(149, 203)
+        TableLayoutPanel1.Name = "TableLayoutPanel1"
+        TableLayoutPanel1.RightToLeft = RightToLeft.Yes
+        TableLayoutPanel1.RowCount = 3
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50.0F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Percent, 50.0F))
+        TableLayoutPanel1.RowStyles.Add(New RowStyle(SizeType.Absolute, 68.0F))
+        TableLayoutPanel1.Size = New Size(473, 223)
+        TableLayoutPanel1.TabIndex = 20
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
+        AutoScaleMode = AutoScaleMode.Font
+        BackColor = SystemColors.Info
+        ClientSize = New Size(776, 457)
+        Controls.Add(TableLayoutPanel1)
+        Controls.Add(Label4)
+        Controls.Add(Label3)
+        Controls.Add(TextBox2)
+        Controls.Add(TextBox1)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Margin = New Padding(3, 4, 3, 4)
+        Name = "Form1"
+        Text = "Form1"
+        TableLayoutPanel1.ResumeLayout(False)
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Button8 As Button
+    Friend WithEvents Button9 As Button
+    Friend WithEvents Button10 As Button
+    Friend WithEvents Button11 As Button
+    Friend WithEvents Button12 As Button
+    Friend WithEvents TextBox2 As TextBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label4 As Label
+    Friend WithEvents TableLayoutPanel1 As TableLayoutPanel
+End Class
